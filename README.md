1f73c32ab2d3c2f018444fcee359f613100cfb60
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2017-06-01 14:44:10 +0100

    Removed weinre

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Project 792 PoINFO sightings app
+This app will NOT work without a Project 792 operator account on Gatekeeper.
--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,18 +1,23 @@
 $(document).on( "pagecontainershow", function( event, ui ) {
 	pageid=ui.toPage[0].id;
+	console.log(ui);
 	allowedpages=['page_signin'];
 	if (!localStorage.signedin) {
 		if (!allowedpages.includes(pageid)) {
 			console.log("Not on allowed page presignin");
 			$.mobile.navigate('signin.html');
+			localStorage.sightings=JSON.stringify([]);
 			return false;
 		};
 	} else {
 		$('.operator_name').html("Operator:"+JSON.parse(localStorage.operator)['id']);
-		if (pageid="page_index") {
+		if (pageid=="page_index") {
 			console.log("Signed in, Redirecting to app page");
 			$.mobile.navigate('app.html');
 			return false;
+		};
+		if (pageid=="page_newsighting") {
+			getlocation();
 		};
 	};
 
@@ -56,6 +61,7 @@ function signin() {
 								clearInterval(launchkey_interval);
 								localStorage.signedin=true;
 								localStorage.operator=JSON.stringify(data.operator);
+								localStorage.jwt=data.jwt;
 								$.mobile.navigate('index.html');
 							} else {
 								launchkey_checking=false;
@@ -91,4 +97,174 @@ function signout() {
 	console.log("Signing out");
 	localStorage.clear();
 	$.mobile.navigate('index.html');
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//photo stuff
+function takephoto(sightingtype,source) {
+	// Retrieve image file location from specified source
+	navigator.camera.getPicture(onPhotoURISuccess, photoonFail, { quality: 100, 
+		destinationType: Camera.DestinationType.FILE_URI,
+		sourceType: source
+	});
+};
+
+function photoonFail(message) {
+	console.log(message);
+	alert('Failed because: ' + message);
+};
+
+function onPhotoURISuccess(imageURI) {
+	console.log(imageURI);
+	$('#newsighting_photourl').val(imageURI);
+};
+
+
+
+
+
+
+
+
+//geolocation stuff
+
+var geoloconSuccess = function(position) {
+	console.log(position);
+	$('.input-latitude').val(position.coords.latitude);
+	$('.input-longitude').val(position.coords.longitude);
+	$('.input-accuracy').val(position.coords.accuracy);
+};
+
+// geoloconError Callback receives a PositionError object
+//
+function geoloconError(error) {
+	console.log(error);
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+};
+
+function getlocation() {
+	navigator.geolocation.getCurrentPosition(geoloconSuccess, geoloconError);
+};
+
+
+
+
+function newsighting_submit() {
+	sighting={};
+
+	sighting.lat=$('#newsighting_lat').val();
+	sighting.lon=$('#newsighting_lon').val();
+	sighting.accuracy=$('#newsighting_accuracy').val();
+	sighting.reg=$('#newsighting_reg').val();
+	sighting.notes=$('#newsighting_notes').val();
+	sighting.photourl=$('#newsighting_photourl').val();
+	sighting.timestamp=Math.floor(Date.now()/1000);
+
+	console.log(sighting);
+	sightings=JSON.parse(localStorage.sightings);
+	sightings.push(sighting);
+	localStorage.sightings=JSON.stringify(sightings);
+	$.mobile.navigate('index.html');
+	alert("Sighting saved locally. Perform sync to save to server.");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function file_fail(e) {
+    console.log("FileSystem Error");
+    console.dir(e);
+	alert("error");
+}
+
+var imagedata;
+var sighting;
+
+function gotFile(fileEntry) {
+
+    fileEntry.file(function(file) {
+        var reader = new FileReader();
+	        reader.onloadend = function(e) {
+		imagedata=this.result;
+		syncsend();
+        }
+	reader.readAsDataURL(file);
+    });
+
+}
+
+
+function loadimage(imageurl) {
+	window.resolveLocalFileSystemURL(imageurl, gotFile, file_fail);
+};
+
+
+
+function sync() {
+	imagedata="";
+	console.log("Syncing");
+	$.mobile.loading( "show", {
+	  text: "Syncing sightings",
+	  textVisible: true,
+	  theme: "a"
+	});
+	
+	if (JSON.parse(localStorage.sightings).length == 0) {
+		console.log("Nothing to sync");
+		synccomplete();
+	} else {
+		sighting=JSON.parse(localStorage.sightings)[0];
+		console.log(sighting);	
+		if (sighting.photourl) {
+			loadimage(sighting.photourl);
+		} else {
+			console.log("Posting now");
+			syncsend();
+		};
+	};
+};
+
+
+function syncsend() {
+	console.log("Sending sighting");
+
+
+	$.post( "https://project792.okonetwork.org.uk/api/poinfo/sightingsubmit", { jwt: localStorage.jwt, sighting: JSON.stringify(JSON.parse(localStorage.sightings)[0]), imagedata: imagedata }).done(function(data) {
+		sightings=JSON.parse(localStorage.sightings);
+		sightings.splice(0, 1);
+		localStorage.sightings=JSON.stringify(sightings);
+		sync();
+	}).error(function(error) {
+		console.log(error);
+		$.mobile.loading("hide");
+		alert("Error communicating with server. Please check network connection.");
+	});
+};
+
+function synccomplete() {
+	$.mobile.loading("hide");
+	alert("Sync complete");
 };
